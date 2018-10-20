0f408cc334cf3389da3b495c8bd5b6e7f9097e48
Bobby Eshleman <bobbyeshleman@gmail.com>
2018-10-20 13:32:05 -0700

    Updated setup linux for bootp and udoo dev

--- setup_linux_dev.sh
+++ setup_linux_dev.sh
@@ -9,12 +9,26 @@ sudo aptitude install gawk wget git diffstat unzip texinfo gcc-multilib \
      build-essential chrpath socat libsdl1.2-dev xterm picocom ncurses-dev lzop \
      gcc-arm-linux-gnueabihf
 
-# For Udoo board, replace with whatever board or BSP you are using
-git clone https://github.com/UDOOboard/linux_kernel
-cd linux_kernel
+sudo aptitude install isc-dhcp-server
+sudo aptitude install tftpd-hpa
+sudo aptitude install nfs-kernel-server
 
-# Replace udoo_neo_defconfig with which ever config you want to use
-ARCH=arm make udoo_neo_defconfig
 
-# Compile the kernel, arm-linux-gnueabihf compiles with the ARM ABI expecting hardware support for floating point units
-ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- make zImage -j5
+# Setup DHCP config
+# /etc/dhcp/dhcpd.conf
+# subnet 10.0.0.0 netmask 255.255.255.0 {
+#   range 10.0.0.0 10.0.0.254;
+# }
+
+# host targetboard {
+#    fixed-address 10.0.0.10;
+     # Ethernet addr of device, from U-BOOT’s $ethaddr
+#    hardware ethernet 00:c0:08:92:4b:9a;
+     #filename "zImage";
+#}
+
+sudo chmod 1777 /var/lib/tftpboot/
+sudo mkdir -m 777 /nfsroot
+sudo service nfs-kernel-server restart
+sudo service isc-dhcp-server restart
+sudo service nfs-kernel-server restart
